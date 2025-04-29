6f9c2588ca11e07e4724b0c25e9229ebbefb5cea
Iván Lanzadera <ivanlanzadera@gmail.com>
2025-04-29 16:00:37 +0200

    Implementados disparos e impactos con asteroides

--- src/app/scenes/GameScene.ts
+++ src/app/scenes/GameScene.ts
@@ -9,6 +9,8 @@ export class GameScene extends Phaser.Scene {
     rightButton?: Phaser.GameObjects.Image;
     isMovingLeft: boolean = false;
     isMovingRight: boolean = false;
+    laserGroup?: Phaser.Physics.Arcade.Group;
+    fireButton?: Phaser.GameObjects.Image;
 
     constructor () {
         super('GameScene');
@@ -45,9 +47,16 @@ export class GameScene extends Phaser.Scene {
         const { width, height } = this.scale;
 
         // FONDO
-        const bg = this.add.image(0, 0, 'bg')
-            .setOrigin(0)
-            .setDisplaySize(width, height);
+        const bg = this.add.image(0, 0, 'bg').setOrigin(0).setDisplaySize(width, height);
+
+        // PUNTUACIONES
+        const username = localStorage.getItem('username') || 'Invitado';
+        const highscore = parseInt(localStorage.getItem(`highscore_${username}`) || '0', 10);
+
+        this.add.text(10, 10, `Highscore: ${highscore}`, {
+            fontSize: '24px',
+            color: '#ffffff'
+        });
 
         // JUGADOR
         this.player = this.physics.add.image(width/2, height - 200, 'player');
@@ -58,7 +67,8 @@ export class GameScene extends Phaser.Scene {
         // CONTROLES
         this.leftButton = this.add.image(width - 100, height - 100, 'cLeft').setScale(0.7).setInteractive();
         this.rightButton = this.add.image(width - 30, height - 100, 'cRight').setScale(0.7).setInteractive();
-        this.add.image(30, height - 100, 'cFire').setScale(0.7).setInteractive();
+        this.fireButton = this.add.image(30, height - 100, 'cFire').setScale(0.7).setInteractive();
+        this.laserGroup = this.physics.add.group(); // Grupo de láseres disparados
 
         this.leftButton.on('pointerdown', () => {
             this.isMovingLeft = true;
@@ -90,6 +100,10 @@ export class GameScene extends Phaser.Scene {
             this.player?.setTexture('player');
         });
 
+        this.fireButton.on('pointerdown', () => {
+            this.fireLaser();
+        });
+
         // METEORITOS
         this.meteorGroup = this.physics.add.group();
         this.meteorTimer = this.time.addEvent({
@@ -100,16 +114,19 @@ export class GameScene extends Phaser.Scene {
         });
         
         this.physics.add.overlap(this.player, this.meteorGroup, this.gameOver, undefined, this);
+        this.physics.add.overlap(this.laserGroup, this.meteorGroup, this.shotMeteor, undefined, this);
     }
 
     override update() {
+        // Eliminamos meteoritos que salen de la pantalla
         this.meteorGroup!.getChildren().forEach((meteor: Phaser.GameObjects.GameObject) => {
             const m = meteor as Phaser.Physics.Arcade.Image;
             if (m.y > this.scale.height + 50) {
-                m.destroy(); // Limpia meteoritos fuera de pantalla
+                m.destroy();
             }
         });
 
+        // Controlamos movimiento de la nave
         const speed = 300;
         if (this.player) {
             if (this.isMovingLeft) {
@@ -119,6 +136,44 @@ export class GameScene extends Phaser.Scene {
             } else {
                 this.player.setVelocityX(0);
             }
+        }
+
+        // Eliminamos los láseres que salen de la pantalla
+        this.laserGroup!.getChildren().forEach((laser: Phaser.GameObjects.GameObject) => {
+            const l = laser as Phaser.Physics.Arcade.Image;
+            if (l.y < -50) {
+                l.destroy();
+            }
+        });
+    }
+
+    fireLaser() {
+        if (!this.player || !this.laserGroup) return;
+
+        const laser = this.laserGroup.create(this.player.x, this.player.y - this.player.height / 2, 'laser') as Phaser.Physics.Arcade.Image;
+        laser.setVelocityY(-500);
+        laser.setData('alive', true);
+    
+        // Efecto óptico del disparo
+        const shoot = this.add.image(this.player.x, this.player.y - this.player.height / 2, 'shot').setScale(0.5);
+        this.time.delayedCall(100, () => shoot.destroy(), [], this);
+    }
+
+    shotMeteor(laser: any, meteor: any) {
+        const m = meteor as Phaser.Physics.Arcade.Image;
+        const l = laser as Phaser.Physics.Arcade.Image;
+
+        // Elimina el láser
+        l.destroy();
+
+        // Resta vida al meteorito
+        let life = m.getData('life') ?? 2;
+        life -= 1;
+
+        if (life <= 0) {
+            m.destroy();
+        } else {
+            m.setData('life', life);
         }
     }
 
@@ -131,8 +186,8 @@ export class GameScene extends Phaser.Scene {
 
         const meteor = this.meteorGroup!.create(x, y, key) as Phaser.Physics.Arcade.Image;
 
-        meteor.setVelocityY(Phaser.Math.Between(100, 150)); // Baja verticalmente
-        meteor.setData('alive', true);
+        meteor.setVelocityY(Phaser.Math.Between(100, 200));
+        meteor.setData('life', 2);
     }
 
     gameOver() {
