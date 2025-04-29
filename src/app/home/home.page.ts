b2c03207134379116bee9730898db9a2dfc569fe
Iván Lanzadera <ivanlanzadera@gmail.com>
2025-04-29 11:22:25 +0200

    Configuración inicial Phaser

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import * as Phaser from 'phaser';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,18 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() {}
+  game?: Phaser.Game;
+  config: Phaser.Types.Core.GameConfig;
+
+  constructor() {
+    this.config = {
+      width: innerWidth,
+      height: innerHeight,
+      parent: "game"
+    }
+  }
+
+  ngOnInit() {
+    this.game = new Phaser.Game(this.config);
+  }
 }
